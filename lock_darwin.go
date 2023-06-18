1ff42efc0401b799dfe9764760dc59e9397db1a2
Faizan Khalid <IamFaizanKhalid@gmail.com>
2023-06-18 11:58:49 +0500

    Removed PList Formatting from ioreg Output

--- lock_darwin.go
+++ lock_darwin.go
@@ -6,8 +6,8 @@ import (
 )
 
 func isScreenLocked() bool {
-	// This will output a plist with session information
-	output, _ := exec.Command("ioreg", "-n", "Root", "-d1", "-a").Output()
+	// This will output registry with session information
+	output, _ := exec.Command("ioreg", "-n", "Root", "-d1").Output()
 
 	// This will check if output["IOConsoleUsers"][0]["CGSSessionScreenIsLocked"] exists
 	return strings.Contains(string(output), "CGSSessionScreenIsLocked")
