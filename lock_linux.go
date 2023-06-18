22488176a7df023622b008023b52b4487c1d62b8
Faizan Khalid <IamFaizanKhalid@gmail.com>
2023-06-18 12:58:59 +0500

    Added AI Generated Commands for Other Desktops

--- lock_linux.go
+++ lock_linux.go
@@ -6,6 +6,7 @@ import (
 )
 
 func isScreenLocked() bool {
+	// WARNING: these are not tested
 	switch os.Getenv("XDG_CURRENT_DESKTOP") {
 	case "Unity":
 		return check(
@@ -13,16 +14,36 @@ func isScreenLocked() bool {
 			"true",
 		)
 	case "KDE":
+		return check(
+			"qdbus org.kde.screensaver /ScreenSaver org.freedesktop.ScreenSaver.GetActive",
+			"true",
+		)
 	case "XFCE":
+		return check(
+			"xfconf-query -c xfce4-session -p /general/LockDialogIsVisible",
+			"true",
+		)
 	case "LXQt":
 	case "MATE":
+		return check(
+			"mate-screensaver-command -q",
+			"is active",
+		)
 	case "Cinnamon":
+		return check(
+			"gdbus call --session --dest org.Cinnamon.ScreenSaver --object-path /org/Cinnamon/ScreenSaver --method org.Cinnamon.ScreenSaver.GetActive",
+			"true",
+		)
 	case "LXDE":
 	case "Deepin":
+		return check(
+			"dbus-send --session --dest=com.deepin.ScreenSaver --type=method_call --print-reply /com/deepin/ScreenSaver com.deepin.ScreenSaver.GetStatus",
+			"true",
+		)
 	default: // default to gnome
 		return check(
 			"gdbus call -e -d org.gnome.SessionManager -o /org/gnome/SessionManager/Presence -m org.freedesktop.DBus.Properties.Get org.gnome.SessionManager.Presence status",
-			"3>",
+			"idle",
 		)
 	}
 }
